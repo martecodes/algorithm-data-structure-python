fe5e0c772d0080a9dc5a95e8c3ab53a4cd1cc263
Jean Marte <jeanmarte93@gmail.com>
2024-05-18 22:07:47 -0400

    BFS

---
+++ graph_algorithm/breadth_first_search.py
@@ -0,0 +1,31 @@
+from collections import deque
+
+def BFS(graph):
+    search_queue = deque()
+    search_queue += graph["you"]
+    
+    while search_queue:
+        person = search_queue.popleft()
+        
+        if person_is_seller(person):
+            print(f"{person} is a mango seller!")
+            return True
+        else:
+            search_queue += graph[person]
+    
+    return False
+
+def person_is_seller(name):
+    return name[-1] == 'm'
+
+graph = {}
+graph["you"] = ["alice", "bob", "claire"]
+graph["bob"] = ["anju", "peggy"]
+graph["alice"] = ["peggy"]
+graph["claire"] = ["thom", "jonny"]
+graph["anju"] = []
+graph["peggy"] = []
+graph["thom"] = []
+graph["jonny"] = []
+
+print(BFS(graph))
