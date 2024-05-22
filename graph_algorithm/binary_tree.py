5fb992bd44a81e2067b9108f4c89d47db2de81be
Jean Marte <jeanmarte93@gmail.com>
2024-05-22 18:19:07 -0400

    BT

---
+++ graph_algorithm/binary_tree.py
@@ -0,0 +1,17 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+
+class BinaryTree:
+    def __init__(self):
+        self.root = None
+    
+    
+    def append(self, value):
+        pass
+    
+    def _append_recursive(self, node, value):
+        pass
